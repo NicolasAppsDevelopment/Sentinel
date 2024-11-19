30416abda0cba9d82f61de0ff0b30c2e45519cd5
root <root@debian12.debian12.virtualbox.org>
2024-11-19 17:09:52 +0100

    creation question entity

---
+++ src/Entity/Question.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statement = null;
+
+    #[ORM\Column]
+    private ?int $answerId1 = null;
+
+    #[ORM\Column]
+    private ?int $answerId2 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $answerId3 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $answerId4 = null;
+
+    #[ORM\Column]
+    private ?int $quizId = null;
+
+    #[ORM\Column]
+    private ?int $type = null;
+
+    #[ORM\Column]
+    private ?int $position = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatement(): ?string
+    {
+        return $this->statement;
+    }
+
+    public function setStatement(string $statement): static
+    {
+        $this->statement = $statement;
+
+        return $this;
+    }
+
+    public function getAnswerId1(): ?int
+    {
+        return $this->answerId1;
+    }
+
+    public function setAnswerId1(int $answerId1): static
+    {
+        $this->answerId1 = $answerId1;
+
+        return $this;
+    }
+
+    public function getAnswerId2(): ?int
+    {
+        return $this->answerId2;
+    }
+
+    public function setAnswerId2(int $answerId2): static
+    {
+        $this->answerId2 = $answerId2;
+
+        return $this;
+    }
+
+    public function getAnswerId3(): ?int
+    {
+        return $this->answerId3;
+    }
+
+    public function setAnswerId3(?int $answerId3): static
+    {
+        $this->answerId3 = $answerId3;
+
+        return $this;
+    }
+
+    public function getAnswerId4(): ?int
+    {
+        return $this->answerId4;
+    }
+
+    public function setAnswerId4(?int $answerId4): static
+    {
+        $this->answerId4 = $answerId4;
+
+        return $this;
+    }
+
+    public function getQuizId(): ?int
+    {
+        return $this->quizId;
+    }
+
+    public function setQuizId(int $quizId): static
+    {
+        $this->quizId = $quizId;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+}
