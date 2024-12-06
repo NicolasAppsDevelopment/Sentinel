2c000ccf7faeab9140a5c8cc076b41dda9c2d0d1
TheAstroCheese <tom.pellecchia1@gmail.com>
2024-12-06 23:03:29 +0100

    fix edit account (save new password)

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,9 +3,11 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Entity\User;
@@ -32,13 +34,19 @@ class AccountController extends AbstractController
     }
 
     #[Route(path: '/edit/{id}', name: 'app_account_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
 
             return $this->redirectToRoute('app_account_view', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
         }
