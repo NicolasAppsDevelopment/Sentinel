685a90ab97c5b76d0937fe86ebfd7e50511a563b
QSimonot <122995270+QSimonot@users.noreply.github.com>
2025-04-08 10:38:54 +0200

    docker

---
+++ php/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# If the vendor directory doesn't exist or you need to ensure it's up to date,
+# you can run composer install here.
+cd /var/www/app-site
+composer install --no-dev
+
+# set permision for symfony
+chmod -R 775 /var/www/app-site
+chown -R www-data:www-data /var/www/app-site
+chmod -R 775 /camera_picture
+chown -R www-data:www-data /camera_picture
+
+# set symbolic link for image upload folder
+rm -R /var/www/app-site/assets/camera_picture
+ln -s /camera_picture /var/www/app-site/assets/camera_picture
+
+
+exec "$@"
