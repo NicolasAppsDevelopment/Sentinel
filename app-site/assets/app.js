9dc4a6519244a0ae76535ead8b110a75208c2db3
Nicolas Colomb <ncolomb@myluckyday.fr>
2025-04-21 15:44:04 +0200

    improve player/view cam UI/UX

--- app-site/assets/app.js
+++ app-site/assets/app.js
@@ -10,12 +10,13 @@ import './fontawesome/css/solid.css';
 import './fontawesome/css/regular.css';
 
 let mobileMenuOpened = false;
+let lastPlayerUITimeout = null;
 
 document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuButton = document.querySelector('.mobile-menu-button');
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    // add event listener to the button
+    // add event listener to the menu button
     mobileMenuButton.addEventListener('click', function () {
         if (mobileMenuOpened) {
             mobileMenu.classList.remove('opened');
@@ -24,4 +25,43 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         mobileMenuOpened = !mobileMenuOpened;
     });
+
+
+    // add event listener to display dropup content on button touched for mobile
+    const dropupDiv = document.querySelector('.dropup');
+    if (dropupDiv) {
+        dropupDiv.addEventListener('touchstart', function () {
+            if (dropupDiv.classList.contains('opened')) {
+                dropupDiv.classList.remove('opened');
+            } else {
+                dropupDiv.classList.add('opened');
+            }
+        });
+    }
+
+
+    // add event listener to display player controls on player touched for mobile
+    const player = document.querySelector('.stream-player-container');
+    if (player) {
+        // display UI at the beginning
+        player.classList.add('display-ui');
+        lastPlayerUITimeout = setTimeout(function () {
+            player.classList.remove('display-ui');
+            lastPlayerUITimeout = null;
+        }, 5000);
+
+
+        player.addEventListener('touchstart', function (e) {
+            if (lastPlayerUITimeout) {
+                clearTimeout(lastPlayerUITimeout);
+            } else {
+                e.preventDefault();
+            }
+            player.classList.add('display-ui');
+            lastPlayerUITimeout = setTimeout(function () {
+                player.classList.remove('display-ui');
+                lastPlayerUITimeout = null;
+            }, 3000);
+        });
+    }
 });
