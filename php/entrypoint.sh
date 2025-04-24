8fa7a155c95a8aeb573655826078150d412200e8
Pellecchia Tom <tom.pellecchia1@gmail.com>
2025-04-25 00:44:27 +0200

    change where stock picture because symfony can't access dir assets

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -8,14 +8,14 @@ composer install
 # set permision for symfony
 chmod -R 775 /var/www/app-site
 chown -R www-data:www-data /var/www/app-site
-chmod -R 775 /camera_picture
-chown -R www-data:www-data /camera_picture
+chmod -R 775 /camera_pictures
+chown -R www-data:www-data /camera_pictures
 
 # set symbolic link for image upload folder
 if [ -d "/var/www/app-site/public/camera_pictures" ]; then
-    rm -R /var/www/app-site/assets/camera_picture
+    rm -R /var/www/app-site/public/camera_pictures
 fi
-ln -s /camera_picture /var/www/app-site/assets/camera_picture
+ln -s /camera_picture /var/www/app-site/public/camera_pictures
 
 # wait MariaDB to be ready
 until mariadb -h "$MYSQL_HOST" -u "$MYSQL_USER" --password="$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
