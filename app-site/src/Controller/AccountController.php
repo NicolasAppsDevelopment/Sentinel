e0ad069f4cd25ca8c896348bd6e2eade0fdc8d7a
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2025-04-27 17:15:41 +0200

    account page (edit + remove)

--- app-site/src/Controller/AccountController.php
+++ app-site/src/Controller/AccountController.php
@@ -9,8 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
-use App\Repository\UserQuizAttemptRepository;
 use App\Form\UserType;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route(path: '/account')]
 class AccountController extends AbstractController
@@ -19,32 +19,11 @@ class AccountController extends AbstractController
         private readonly EntityManagerInterface $entityManager
     ) {}
 
-    // #[Route(path: '/view/{id}', name: 'app_account_view', methods: ['GET'])]
-    // public function view(User $user): Response
-    // {
-    //     //$nbQuestionsAnswered = $userQuizAttemptRepository->getUserNbQuestionsAnswered($user);
-    //     $percentageOfCorrectAnswers = "No questions answered";
-
-    //     //$nbAnswerSubmitted = $userQuizAttemptRepository->getUserNbAnswerSubmited($user);
-
-    //     // if (count($nbQuestionsAnswered) != 0) {
-    //     //     $percentageOfCorrectAnswers = round((count($userQuizAttemptRepository->getUserNbQuestionsAnsweredCorrectly($user)) * 100) / count($nbAnswerSubmitted), 0, PHP_ROUND_HALF_UP);
-    //     //     $percentageOfCorrectAnswers = $percentageOfCorrectAnswers . "%";
-    //     // }
-
-    //     // return $this->render('account/view.html.twig', [
-    //     //     'user' => $user,
-    //     //     'NbQuizzesPlayed' => count($userQuizAttemptRepository->getUserNbQuizzesPlayed($user)),
-    //     //     'NbQuestionsAnswered' => count($nbQuestionsAnswered),
-    //     //     'PercentageOfCorrectAnswers' => $percentageOfCorrectAnswers,
-    //     // ]);
-    // }
-
-    #[Route(path: '/remove', name: 'app_account_remove', methods: ['POST'])]
+    #[Route(path: '/remove', name: 'app_account_remove', methods: ['GET','POST'])]
     public function remove(): Response
     {
         if (!$this->getUser()) {
-            return $this->redirectToRoute('app_register', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_logout');
         }
 
         $this->entityManager->remove($this->getUser());
@@ -52,7 +31,7 @@ class AccountController extends AbstractController
 
         // TODO: remove user in cache
 
-        return $this->redirectToRoute('app_register', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_logout');
     }
 
     #[Route(path: '/edit/{id}', name: 'app_account_edit', methods: ['GET', 'POST'])]
@@ -64,13 +43,16 @@ class AccountController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $plainPassword = $form->get('plainPassword')->getData();
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            if ($plainPassword) {
+                // encode the plain password
+                $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            }
+
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_account_view', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_account_edit', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('account/edit.html.twig', [
