be70cfa53be5609633324165316fd30ff2ce0121
Nicolas Colomb <ncolomb@myluckyday.fr>
2025-04-28 23:33:44 +0200

    rssi refacto

--- app-site/src/Entity/Custom/Rssi.php
+++ app-site/src/Entity/Custom/Rssi.php
@@ -2,10 +2,6 @@
 
 namespace App\Entity\Custom;
 
-use App\Dto\ActionDeviceStatusDto;
-use App\Dto\CameraDeviceStatusDto;
-use App\Entity\Couple;
-
 class Rssi
 {
     public int $value;
@@ -15,29 +11,16 @@ class Rssi
     public function __construct(int $rssiValue)
     {
         $this->value = $rssiValue;
-        $this->state = $this->toString($rssiValue);
-        $this->color = $this->toColor($rssiValue);
-    }
 
-    public function toString(int $rssi): string
-    {
-        if ($rssi > -50) {
-            return 'Excellent';
-        } elseif ($rssi > -70) {
-            return 'Good';
+        if ($rssiValue > -50) {
+            $this->state = 'Excellent';
+            $this->color = 'green';
+        } elseif ($rssiValue > -70) {
+            $this->state = 'Good';
+            $this->color = 'orange';
         } else {
-            return 'Weak';
-        }
-    }
-
-    public function toColor(int $rssi): string
-    {
-        if ($rssi > -50) {
-            return 'green';
-        } elseif ($rssi > -70) {
-            return 'orange';
-        } else {
-            return 'red';
+            $this->state = 'Weak';
+            $this->color = 'red';
         }
     }
 }
