d10f76da3c7fdc871508d1ac8abc0f47622ab0d0
QSimonot <122995270+QSimonot@users.noreply.github.com>
2025-04-29 22:35:40 +0200

    formulaire fonctionnelle (mais faut tester)

--- app-site/src/Controller/SettingsController.php
+++ app-site/src/Controller/SettingsController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Process\Process;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Symfony\Component\HttpFoundation\Request;
 
 final class SettingsController extends AbstractController
 {
@@ -22,19 +22,28 @@ final class SettingsController extends AbstractController
         ]);
     }
 
-    #[Route('/settings/sync-time', name: 'app_settings_sync_time')]
-    public function syncTime(): Response
+    #[Route('/settings/sync-time', name: 'app_settings_sync_time', methods: ['POST'])]
+    public function syncTime(Request $request): Response
     {
-        $process = new Process(['sudo', 'date', '-s', 'true']);
-        $process->run();
+        // Validate the date format (Y-m-d H:i:s)
+        $datetime = $request->request->get('server-time'); 
+        if ($datetime) {
+            // Formater correctement la date pour la commande `date`
+            $dateFormatted = date('Y-m-d H:i:s', strtotime($datetime));
+            $process = new Process(['sudo', 'date', '-s', $dateFormatted]);
+            $process->run();
+
+
 
         if (!$process->isSuccessful()) {
             return new Response('Erreur lors de la synchronisation de l\'heure : ' . $process->getErrorOutput(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
         return new Response('Heure synchronisée avec succès.');
-    }
+        }  
+        return new Response('Date invalide.');
 
+    }
     #[Route('/settings/server-time', name: 'app_settings_server_time')]
     public function serverTime(): Response
     {
