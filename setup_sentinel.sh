a05c0d84857da9dd443a2e6ef3a8c059f744e75b
PELLECCHIA TOM p2204947 <tom.pellecchia@etu.univ-lyon1.fr>
2025-05-03 22:54:27 +0000

    Edit setup_sentinel.sh

--- setup_sentinel.sh
+++ setup_sentinel.sh
@@ -122,9 +122,5 @@ systemctl restart systemd-networkd
 systemctl restart hostapd
 systemctl restart dnsmasq
 
-# Enable NetworkManager
-systemctl enable NetworkManager
-systemctl start NetworkManager
-
 # Final reboot
 reboot
